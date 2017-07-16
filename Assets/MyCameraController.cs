06139d4283814d47bae9f68251617ca44fd0fbe7
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2017-07-16 20:11:56 +0900

    first commit user contoroll

---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour {
+
+    //Playerオブジェクト
+    private GameObject Player;
+
+    //Playerとカメラの距離
+    private float difference;
+
+
+    // Use this for initialization
+    void Start () {
+
+        this.Player = GameObject.Find("Player");
+
+        //Plaerとカメラの位置（z座標）の差を求める
+        this.difference = Player.transform.position.z - this.transform.position.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Playerの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.Player.transform.position.z - difference);
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //移動させるコンポーネントを入れる
+    private Rigidbody myRigidbody;
+
+    //前進するための力
+    private float forwardForce = 400.0f;
+
+    //後進するための力
+    private float backwardForce = 800.0f;
+
+    //横に進むための力
+    private float sidewardForce = 800.0f;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Rigidbodyコンポーネントを取得
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //前方向の力を加える
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+
+    }
+}
